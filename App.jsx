a7a3769e02b238adf341caf2e4fe3fb7ca689fe9
Awais Azam <168289709+awais296azam@users.noreply.github.com>
2026-02-17 18:18:33 +0500

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -27,18 +27,45 @@ import InlineStyle from './InlineStyle'
 import Produduts from './Produduts'
 import Studentcard from './Studentcard'
 import Stylecomp from './Stylecomp'
+import Bootstrap from './Bootstrap'
+import Dropddown from './Dropddown'
+import Product from './Product'
+import { Navbar } from 'react-bootstrap'
+import NaveBar from './Navebar'
+import UseRef from './UseRef'
+import Uncantroledcomp from './Uncantroledcomp'
+import Functionasprop from './Functionasprop '
+import Parent from './Parent'
+import Formststus from './Formststus'
+import Deridestste from './Deridestste'
+import Child1 from './Child1'
+import Child2 from './Child2'
+import React, { UseState } from 'react'
+import ObjectState from './ObjectState'
+import Actionststes from './Actionststes'
+import Useidhook from './Useidhook'
+import UseIdhook from './Useidhook'
 
 
 
+
+let Guest=0
 function App() {
+  const[User,Setuser]=useState('');
+
+  function ShowName() {
+   
+    alert("This is user Name")
+  }
   const [Display, SetDisplay] = useState(true);
-  const [count, setCount] = useState(0)
-  let Univeristy= ['UET','UMT','MUL','PU','Agri'];
-const [empName,setEmpName] = useState("awais azam");
+  const [Count, SetCount] = useState(0)
+  let Univeristy = ['UET', 'UMT', 'MUL', 'PU', 'Agri'];
+  const [EmpName, SetEmpName] = useState("awais azam");
+ 
   return (
     <>
       <div>
-        
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -48,51 +75,74 @@ const [empName,setEmpName] = useState("awais azam");
       </div>
       <h1>Vite + React</h1>
       
-      
-      <Eventfunction></Eventfunction>
+
+      {/* <Eventfunction></Eventfunction>
       <City></City>
       <Compo></Compo>
       <Wilde></Wilde>
       <Jscode></Jscode>
       <Assignment></Assignment>
-      <Arraywithprop uni = {Univeristy[0]}/>
-       <Arraywithprop uni = {Univeristy[1]}/>
-       <Arraywithprop uni = {Univeristy[2]}/>
-       <Arraywithprop uni = {Univeristy[3]}/>
-       <Arraywithprop uni = {Univeristy[4]}/>
-       <Usestateprpos emp={empName} setEmpName={setEmpName} />
-       <button onClick={()=>setEmpName("Azam")}>Change Name</button>
-       <Propdefaultvalue userName="Awais"/>
-       <Propdefaultvalue userName="Azam"/>
-       <Propdefaultvalue />
-       <Inputboxdata></Inputboxdata>
-       <ControlComponent />
-    <Spreadinreact /> 
-    <Cheakbox />
-    <Radioanddrop />  
-    <Tableandarray /> 
-    <Nestedloop />
-    <Currentdate />
-   <Dropdown/>
-   <Black/>
-   <Lifecycle />
-   <InlineStyle /> 
-   <Produduts /> 
-    <Studentcard />
-    <Stylecomp />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Arraywithprop />
+      <Usestateprpos emp={EmpName} setEmpName={SetEmpName} />
+      <button onClick={() => SetEmpName("Azam")}>Change Name</button>
+      <Propdefaultvalue />
+      <Propdefaultvalue />
+      <Inputboxdata></Inputboxdata>
+      <ControlComponent />
+      <Spreadinreact />
+      <Cheakbox />
+      <Radioanddrop />
+      <Tableandarray />
+      <Nestedloop />
+      <Currentdate />
+      <Dropdown />
+      <Black />
+      <Lifecycle />
+      <InlineStyle />
+      <Produduts />
+      <Studentcard />
+      <Stylecomp />
+      <NaveBar />
+      <Bootstrap />
+      <Dropddown />
+      <Product />
+      <UseRef />
+      <Uncantroledcomp />
+      <Functionasprop showName={ShowName} />
+      <Parent />
+      <Formststus />
+      <Gueste />
+      <Deridestste/>
+      <Child1 Setuser={Setuser}/>
+      <Child2 User={User}/>
+      <ObjectState/>
+      <Actionststes /> */}
+      <Useidhook/>
+      <hr />
+      <UseIdhook/>
+       <hr />
+       <UseIdhook/>
+      {/* <div 
+      className="card"
+      >
+        <button onClick={() => SetCount((count) => count + 1)}>
+          count is {Count}
         </button>
         <p>
           Created by Awais Azam
         </p>
-      </div>
-      <p className="read-the-docs">
+      </div> */}
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
-
+// const Gueste=()=>{
+//   Guest=Guest+1;
+//   return (
+//     <h1> we have {Guest} guest we need {Guest} cup of tea.</h1>
+//      );
+//     }
+   
 export default App
